c66052ae2c0a7518596a619285a01fee9bff2d1d
Aubrey Schott <brilovescoding@gmail.com>
2024-11-06 12:59:52 -0500

    Re-added component and now it's working???

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import { Snippet } from "@nextui-org/snippet";
 import { Code } from "@nextui-org/code";
 import { Shout } from "@/components/shout";
+import { AchievementsContainer } from "@/components/achievements";
 
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <Shout />
+
+      <AchievementsContainer />
 
       <div className="mt-8">
         <Snippet hideCopyButton hideSymbol variant="bordered">

56f8ba791e9e23f0e69cec26261ba23090d90fd9
Sam Zack <103949002+szack25@users.noreply.github.com>
2024-11-12 13:38:47 -0500

    Started admin points component

--- firebase-configuration/firebaseDatabase.ts
+++ firebase-configuration/firebaseDatabase.ts
@@ -1,16 +1,12 @@
 import { collection, getDoc, getDocs, getFirestore } from 'firebase/firestore';
 import app from './firebaseApp';
-import { useState } from 'react';
 
 const db = getFirestore(app);
 
-export const [allHousesLeaderboard, setAllHousesLeaderboard] = useState<{ house: any; points: any; rank: any; }[]>([]);
-export const [allIndividualLeaderboard, setAllIndividualLeaderboard] = useState<{ name: any; points: any; rank: any; }[]>([]);
-
-export const getAllHousesLeaderboardData = async () => {
+export const getAllHousesLeaderboardData = async (): Promise<{ house: string; points: number[]; rank: number; }[]> => {
     const housesCollection = collection(db, 'Leaderboard/LeaderboardId/houseRankings');
+    var leaderboardData: { house: string; points: Array<number>; rank: number; }[] = [];
     getDocs(housesCollection).then((querySnapshot) => {
-                var leaderboardData: { house: any; points: any; rank: any; }[] = [];
                 querySnapshot.forEach((doc) => {
                     // convert data into json
                     const data = doc.data();
@@ -20,23 +16,26 @@ export const getAllHousesLeaderboardData = async () => {
                         rank: data.rank
                     });
                 });
-                setAllHousesLeaderboard(leaderboardData);
             });
+            return leaderboardData
 }
 
-export const getAllIndividualData = async () => {
+export const getAllIndividualData = async (): Promise<{ name: string; points: number[]; rank: number; house: string; grade: number; }[]> => {
     const individualCollection = collection(db, 'Leaderboard/LeaderboardId/memberRankings');
+    var individualData: { name: string; points: Array<number>; rank: number; house: string, grade: number }[] = [];
     getDocs(individualCollection).then((querySnapshot) => {
-                var individualData: { name: any; points: any; rank: any; }[] = [];
+                var individualData: { name: string; points: Array<number>; rank: number; house: string, grade: number }[] = [];
                 querySnapshot.forEach((doc) => {
                     // convert data into json
                     const data = doc.data();
                     individualData.push({
                         name: data.name,
                         points: data.points,
-                        rank: data.rank
+                        rank: data.houseRank,
+                        house: data.house,
+                        grade: data.grade
                     });
                 });
-                setAllIndividualLeaderboard(individualData);
             });
+            return individualData;
 }

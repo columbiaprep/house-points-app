da7ffebb860531c8a1736da380f9cebb75a65c21
Sam Zack <103949002+szack25@users.noreply.github.com>
2024-11-14 08:45:01 -0500

    Checkpoint

--- firebase-configuration/firebaseDatabase.ts
+++ firebase-configuration/firebaseDatabase.ts
@@ -1,41 +1,57 @@
-import { collection, getDoc, getDocs, getFirestore } from 'firebase/firestore';
+import { addDoc, collection, getDoc, getDocs, getFirestore } from 'firebase/firestore';
 import app from './firebaseApp';
 
 const db = getFirestore(app);
 
 export const getAllHousesLeaderboardData = async (): Promise<{ house: string; points: number[]; rank: number; }[]> => {
-    const housesCollection = collection(db, 'Leaderboard/LeaderboardId/houseRankings');
-    var leaderboardData: { house: string; points: Array<number>; rank: number; }[] = [];
-    getDocs(housesCollection).then((querySnapshot) => {
-                querySnapshot.forEach((doc) => {
-                    // convert data into json
-                    const data = doc.data();
-                    leaderboardData.push({
-                        house: data.house,
-                        points: data.points,
-                        rank: data.rank
-                    });
-                });
-            });
-            return leaderboardData
+  const housesCollection = collection(db, 'houseRankings');
+  const querySnapshot = await getDocs(housesCollection);
+  const leaderboardData: { house: string; points: number[]; rank: number; }[] = [];
+  querySnapshot.forEach((doc) => {
+    const data = doc.data();
+    leaderboardData.push({
+      house: data.house,
+      points: data.points,
+      rank: data.rank,
+    });
+  });
+  return leaderboardData;
 }
 
 export const getAllIndividualData = async (): Promise<{ name: string; points: number[]; rank: number; house: string; grade: number; }[]> => {
-    const individualCollection = collection(db, 'Leaderboard/LeaderboardId/memberRankings');
-    var individualData: { name: string; points: Array<number>; rank: number; house: string, grade: number }[] = [];
-    getDocs(individualCollection).then((querySnapshot) => {
-                var individualData: { name: string; points: Array<number>; rank: number; house: string, grade: number }[] = [];
-                querySnapshot.forEach((doc) => {
-                    // convert data into json
-                    const data = doc.data();
-                    individualData.push({
-                        name: data.name,
-                        points: data.points,
-                        rank: data.houseRank,
-                        house: data.house,
-                        grade: data.grade
-                    });
-                });
-            });
-            return individualData;
+  const individualCollection = collection(db, 'individuals');
+  const querySnapshot = await getDocs(individualCollection);
+  const individualData: { name: string; points: number[]; rank: number; house: string; grade: number; }[] = [];
+  querySnapshot.forEach((doc) => {
+    const data = doc.data();
+    individualData.push({
+      name: data.name,
+      points: data.points,
+      rank: data.houseRank,
+      house: data.house,
+      grade: data.grade,
+    });
+  });
+  return individualData;
 }
+
+export const writeDummyDataForIndividuals = async () => {
+  const individualCollection = collection(db, 'individuals');
+  // Houses are: Red, Blue, Green, Gold, Silver, Pink, Purple, Orange
+  // Automatically generate 100 dummy data
+  for (let i = 0; i < 100; i++) {
+    const docRef = await addDoc(individualCollection, {
+      name: `Student ${i}`,
+      points: [
+        Math.floor(Math.random() * 100),
+        Math.floor(Math.random() * 100),
+        Math.floor(Math.random() * 100),
+      ],
+      houseRank: Math.floor(Math.random() * 8),
+      house: ['Red', 'Blue', 'Green', 'Gold', 'Silver', 'Pink', 'Purple', 'Orange'][Math.floor(Math.random() * 8)],
+      grade: Math.floor(Math.random() * 12) + 1,
+    });
+    console.log("Document written with ID: ", docRef.id);
+  }
+}
+

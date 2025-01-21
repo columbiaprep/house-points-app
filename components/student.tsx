652de508a4f206d2ee4731706bd0060a548095c7
Aubrey Schott <brilovescoding@gmail.com>
2025-01-21 10:50:16 -0500

    Individual points component for testing

--- components/student.tsx
+++ components/student.tsx
@@ -1,18 +1,21 @@
 "use client";
 
 import { useState } from "react";
-import { Button } from "@nextui-org/button";
 import {Card, CardHeader, CardBody, CardFooter, Divider, Link, Image} from "@nextui-org/react";
-import axios from 'axios'
+import { useAuth } from '@/contexts/AuthContext';
+import { fetchIndividual, IndividualDocument } from "@/firebase-configuration/firebaseDb";
 
-export const StudentIndividualPoints = () => {
-      //get the data from the API
-      //store it locally
 
-      //render the student points component
-      //insert the points value for that
-      //student into the component
-      //const [housePoints, setHousePoints] = useState(100);
+export const StudentIndividualPoints = async () => {
+  const user = useAuth().user
+
+  if (!user || !user.email) {
+    throw new Error('User email is not available');
+  }  
+  const userDoc = await fetchIndividual(user.email)
+  const totalPoints = userDoc.beingGoodPts + userDoc.attendingEventsPts + userDoc.sportsTeamPts;
+  //TODO: Change how totalpoints is calculated once
+  //backend is updated to have a totalpoints field
       
       return (
         <Card className="max-w-[400px]">
@@ -23,8 +26,7 @@ export const StudentIndividualPoints = () => {
           <Divider />
 
           <CardFooter className="flex flex-col items-center">
-            {/* <p className="text-xl font-bold">{housePoints}</p> */}
-            <h3>100</h3>
+            <h3>{totalPoints}</h3>
           </CardFooter>
         </Card>
       );

d1fb35da01bf0da530f93fa1937e275f0582c4c3
Wyatt <wsneigut@icloud.com>
2025-04-10 13:24:25 -0400

    april 10

---
+++ app/becktest/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import IndividualHouseComponent from "/components/IndividualHouseComponent.tsx";
+
+const BeckTest = () => {
+    const router = useRouter();
+
+    return (
+        <div>
+            <h1>HI!</h1>
+            <IndividualHouseComponent /> 
+        </div>
+    );
+}
+
+export default BeckTest;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,55 +1,10 @@
-const { heroui } = require("@heroui/react");
-const { Quicksand, Ledger } = require("next/font/google");
-
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@heroui/react/**/*.{js,ts,jsx,tsx}", // 👈 this is key for HeroUI
   ],
-  darkMode: "class", // This enables dark mode support
   theme: {
-    extend: {
-      colors: {
-        gold: "#FFD700", // Custom gold color for glowing effects
-      },
-      boxShadow: {
-        'gold-glow': '0 0 15px rgba(255, 255, 255, 0.8)', // Custom gold glow shadow for leaderboard tablet
-      },
-      fontFamily: {
-        sans: ['Quicksand', 'sans-serif'], // Use Quicksand font from Google Fonts
-        ledger: ['Ledger', 'serif'], // Use Ledger font from Google Fonts
-      },
-      animation: {
-        popOut: 'popOut 0.3s ease-out', // The animation for popping out
-      },
-      keyframes: {
-        popOut: {
-          '0%': { transform: 'scale(1)', opacity: '0.8' },
-          '50%': { transform: 'scale(1.1)', opacity: '1' },
-          '100%': { transform: 'scale(1)', opacity: '1' },
-        },
-      },
-    },
+    extend: {},
   },
-  plugins: [
-    heroui({
-      defaultTheme: "light",
-      themes: {
-        light: {
-          colors: {
-            foreground: "rgb(4, 11, 15)",
-            background: "rgb(243, 243, 243)",
-          },
-        },
-        dark: {
-          colors: {
-            foreground: "rgb(202, 203, 203)",
-            background: "rgb(4, 11, 15)",
-          },
-        },
-      },
-    }),
-  ],
-};
+  plugins: [],
+}

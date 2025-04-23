766ac63bcbcd3012688218121763b179d9cca5f9
Sebastian <srosado26@cgps.org>
2025-04-23 08:51:17 -0400

    updating spreads

--- app/sebastian-test/spread/page.tsx
+++ app/sebastian-test/spread/page.tsx
@@ -6,9 +6,10 @@ import { Doughnut } from "react-chartjs-2";
 import { Pie } from 'react-chartjs-2';
 
 
+
 ChartJS.register(ArcElement, Tooltip, Legend);
 export const options = {
-    scales: {
+    scales: {  
       y: {
         beginAtZero: true,
       },
@@ -26,11 +27,11 @@ export const housePointsSpread = {
         label: '# of Ponts',
         data: [12, 19, 3, 5, 2],
         backgroundColor: [
-            'rgba(71, 204, 98, 0.5)',
-            'rgba(71, 181, 93, 0.5)',
-            'rgba(46, 161, 33, 0.5)',
-            'rgba(17, 136, 25, 0.5)',
-            'rgba(6, 61, 5, 0.5)',
+            'rgba(108, 245, 136, 0.86)',
+            'rgba(40, 213, 75, 0.91)',
+            'rgba(26, 187, 8, 0.7)',
+            'rgba(17, 136, 21, 0.7)',
+            'rgba(6, 61, 5, 0.7)',
             'rgba(255, 159, 64, 0.2)',
           ],
           borderColor: [
@@ -56,9 +57,9 @@ export const housePointsSpread = {
         label: '# of Points',
         data: [30,5,9],
         backgroundColor: [
-            'rgba(71, 204, 98, 0.5)',
-            'rgba(71, 181, 93, 0.5)',
-            'rgba(46, 161, 33, 0.5)',
+            'rgba(22, 236, 64, 0.6)',
+            'rgba(1, 170, 35, 0.6)',
+            'rgba(10, 94, 27, 0.6)',
             'rgba(17, 136, 25, 0.5)',
             'rgba(6, 61, 5, 0.5)',
             'rgba(255, 159, 64, 0.2)',
@@ -81,8 +82,11 @@ export const housePointsSpread = {
   export default function Home() {
     const router = useRouter();
     return (
-        <div style={{ height: '300px' }}> {/* Changing the hight of it, most likely will do this later when adding the different componets and not here*/}
-
+        <div className="bg-green-200 grid place-items-center font-stretch-150% font-mono font-bold text-3xl"> {/* Changing the hight of it, most likely will do this later when adding the different componets and not here*/}
+<div>
+<p className="grid place-items-center">
+        HOUSE SPREAD
+</p>
 <Doughnut 
   data={housePointsSpread} 
   options={{ 
@@ -101,6 +105,14 @@ export const housePointsSpread = {
     },
   }} 
 />
+</div>
+<div>
+    
+</div>
+<div>
+<p className="grid place-items-center mt-10">  {/* mt-10 adds space between the two graphs*/}
+        Personal Spread
+</p>
 <Pie 
   data={personalPointsSpread} 
   options={{ 
@@ -112,7 +124,7 @@ export const housePointsSpread = {
     },
   }} 
 />
-            
+</div>            
 </div>
     )
 }

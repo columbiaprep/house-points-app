e941df24040efeca8d8d60e4a9117fe663b66006
Aubrey Schott <brilovescoding@gmail.com>
2025-05-05 15:56:00 -0400

    Updating app/page.tsx by erasing unused code

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,17 @@
 "use client";
-import { Snippet } from "@heroui/snippet";
-import { Code } from "@heroui/code";
+
 import { useRouter } from "next/navigation";
 
-import { Shout } from "@/components/shout";
-import { AchievementsContainer } from "@/components/achievements";
-
 export default function Home() {
+    //TODO: Combine this with Dashboard or remove it, since it's just 
+    //moving us into /dashboard anyway
     const router = useRouter();
 
     router.push("/dashboard");
 
     return (
-        <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-            <Shout />
+        <section>
 
-            <AchievementsContainer />
-
-            <div className="mt-8">
-                <Snippet hideCopyButton hideSymbol variant="bordered">
-                    <span>
-                        Get started by editing{" "}
-                        <Code color="primary">app/page.tsx</Code>
-                    </span>
-                </Snippet>
-            </div>
         </section>
     );
 }

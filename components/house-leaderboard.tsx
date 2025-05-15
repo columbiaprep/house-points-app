773ce15fa5a64ee6ead5cf7f4f7b104a5f91080d
Aubrey Schott <brilovescoding@gmail.com>
2025-05-15 14:39:39 -0400

    Passing student props to the house-leaderboard component. Also cleaned up file tree

--- components/house-leaderboard.tsx
+++ components/house-leaderboard.tsx
@@ -2,13 +2,12 @@ import Image from "next/image";
 import { useState, useEffect } from "react";
 import { Spinner } from "@heroui/react";
 
-import { fetchAllHouses } from "@/firebase-configuration/firebaseDb";
+import { fetchAllHouses, IndividualDocument } from "@/firebase-configuration/firebaseDb";
 import { HouseDocument } from "@/firebase-configuration/firebaseDb";
 import {Link} from "@heroui/link";
 
 export const HousePointsRow: React.FC<HouseDocument> = ({id, place, name, colorName, accentColor, houseImage, totalPoints}) => {
     const gradientClasses = `from-${colorName}-400 to-${accentColor}-700 outline-${accentColor}-900 shadow-${colorName}-500/50`
-    console.log(gradientClasses)
     return (
             <Link 
                 className={`grid min-w-400 max-h-200 place-items-center ${gradientClasses} shadow-lg items-center flex rounded-xl bg-gradient-to-r mb-4 hover:outline-4 `}
@@ -20,7 +19,6 @@ export const HousePointsRow: React.FC<HouseDocument> = ({id, place, name, colorN
                     <div className="ml-3 text-xl align-middle basis-1/10 font-stretch-100% font-mono font-bold">
                         {place}
                     </div>
-                    {/* Also the distance between House name and Points would change once I could get it to be smaller then the whole page */}
                     <Image
                     className="ms-2 me-2 object-center object-contain basis-2/10 dark:invert"
                     src= {"/houseImages/"+ houseImage + ".png"}
@@ -30,24 +28,19 @@ export const HousePointsRow: React.FC<HouseDocument> = ({id, place, name, colorN
                     priority
                     />
                 
-                    <p className ={`ml-1 text-center text-2xl align-middle basis-5/10 flex-grow font-stretch-100% font-mono font-bold flex-1 overflow-hidden text-ellipsis`}>
+                    <p className ={`ml-1 text-wrap text-center text-2xl align-middle basis-5/10 flex-grow font-stretch-100% font-mono font-bold flex-1 overflow-hidden text-ellipsis`}>
                         {name} 
                     </p>
 
                     <p className ="min-w-0 ml-1 me-2 text-xs basis-2/10 font-mono flex-shrink">
                         Points:<br /><span className="text-xl font-bold">{totalPoints}</span>
                     </p>
-
-                
-                    {/* Spacing between different houses */}
-
             </Link>
-                      
     )
 }
 
 
-export const HousePointsContainer = () => {
+export const HousePointsContainer: React.FC<IndividualDocument> = (student: IndividualDocument) => {
     //anything I'm fetching needs to use the below format
     //import the appropriate interface from the database
     const [houses, setHouses] = useState<HouseDocument[]>([]);
@@ -77,6 +70,7 @@ export const HousePointsContainer = () => {
             (   
             <div className="min-w-min w-1/2"> 
                 {houses.map((house, index) => (
+                    
                     <HousePointsRow 
                         key = {index} 
                         name={house.name} 
@@ -86,10 +80,8 @@ export const HousePointsContainer = () => {
                         totalPoints={house.totalPoints}
                         id={house.name}
                         place={house.place}
-                        
+                        isStudentHouse={house.name == student.name}
                         />
-                    
-                    
                 ))}
             </div>
             )

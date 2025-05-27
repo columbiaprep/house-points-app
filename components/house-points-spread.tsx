86150c84147580f38b0142f779572e51d90ff82f
Sebastian <srosado26@cgps.org>
2025-05-26 20:46:03 -0400

    background change for houseColor

---
+++ components/house-points-spread.tsx
@@ -0,0 +1,72 @@
+"use client";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { Spinner } from "@heroui/react";
+import {useAuth} from "@/contexts/AuthContext"
+import { fetchAllHouses, IndividualDocument } from "@/firebase-configuration/firebaseDb";
+import { HouseDocument } from "@/firebase-configuration/firebaseDb";
+import {Link} from "@heroui/link";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut, Pie } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+export const options = {
+    scales: {  
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+  
+export const housePointsSpread = {
+    title: {
+        display: true,
+        text: 'House Points Spread'
+    },
+    labels: ['School Events', 'House Challenges', 'House Drives', "My Points", "Others Points"],
+    datasets: [
+      {
+        label: '# of Ponts',
+        data: [12, 19, 3, 5, 50],
+        backgroundColor: [
+            'rgba(173, 216, 230, 0.86)',
+            'rgba(100, 180, 255, 0.91)', 
+            'rgba(70, 130, 180, 0.7)', 
+            'rgba(40, 90, 150, 0.7)', 
+            'rgba(10, 30, 80, 0.7)',
+          ],
+          borderColor: [
+            'rgba(30, 100, 160, 1)',
+   
+        ],
+        borderWidth: 3,
+      },
+    ],
+  };
+  export const personalPointsSpread = {
+    title: {
+        display: true,
+        text: 'Personal Points Spread'
+    },
+    labels: ["Caught Being Good", "Support (attending events)", "Participation (season long extracurriculars)","Attendance","Community Service", "Events"],
+    datasets: [
+      {
+        label: '# of Points',
+        data: [30,5,9,16,20,22],
+        backgroundColor: [
+            'rgba(100, 200, 255, 0.94)',
+            'rgba(70, 170, 240, 0.89)',
+            'rgba(50, 140, 220, 0.85)', 
+            'rgba(30, 110, 200, 0.8)', 
+            'rgba(20, 90, 180, 0.75)', 
+            'rgba(10, 60, 150, 0.7)',
+          ],
+          borderColor: [
+           'rgba(30, 100, 160, 1)',
+
+      
+        ],
+        borderWidth: 3,
+      },
+    ],
+  };
